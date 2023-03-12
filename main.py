fdea336edcb365e4366f9ce5e06ba73d5c0c9c18
Kredis21354 <125788986+Kredis21354@users.noreply.github.com>
2023-03-12 07:33:05 +0000

    added nat

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 from colorama import Fore
 
 class Person:
-  def __init__ (self):
-    r = requests.get("https://randomuser.me/api/")
+  def __init__ (self, nat):
+    r = requests.get("https://randomuser.me/api/?nat=" + nat)
     res = r.json()
     self.name = res["results"][0]["name"]["first"]
     self.surename = res["results"][0]["name"]["last"]
@@ -11,12 +11,21 @@ class Person:
     self.phone = res["results"][0]["phone"]
     self.email = res["results"][0]["email"]
     self.isMale = res["results"][0]["gender"] == "male"
-    
-    
 
-
-
+  def print(self):
+    if self.isMale:
+      print(Fore.CYAN)
+    else:
+      print(Fore.MAGNETA)
+    print(self.name)
+    print(self.surename)
+    print(self.age)
+    print(self.phone)
+    print(self.email)
+    print(self.isMale)
+  
 random_person = Person()
+random_person.print
 
 if random_person.isMale:
   print(Fore.CYAN)
@@ -28,3 +37,4 @@ print(random_person.age)
 print(random_person.phone)
 print(random_person.email)
 print(random_person.isMale)
+
